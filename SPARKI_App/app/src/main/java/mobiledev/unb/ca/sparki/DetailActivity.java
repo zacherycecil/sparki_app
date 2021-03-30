1ec5f261b7f57d7855bf3b18e3b391d027584f60
Zachery Lewis <zcjl98@yahoo.ca>
2021-03-30 10:59:58 -0300

    Changed Charac teristic value

--- SPARKI_App/app/src/main/java/mobiledev/unb/ca/sparki/DetailActivity.java
+++ SPARKI_App/app/src/main/java/mobiledev/unb/ca/sparki/DetailActivity.java
@@ -174,7 +174,7 @@ public class DetailActivity extends AppCompatActivity {
             }
             else {
                 statusString += "\nSERVICE NOT NULL";
-                BluetoothGattCharacteristic characteristic = service.getCharacteristic(UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"));
+                BluetoothGattCharacteristic characteristic = service.getCharacteristic(UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"));
                 characteristic.setValue("FF");
                 bluetoothGatt.writeCharacteristic(characteristic);
             }
